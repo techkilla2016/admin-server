36e6d14e2bcd539d0d8bef96b903a3a9d879c0c2
w3App <kshatriyakabawat@gmail.com>
2023-06-01 09:11:29 +0530

    remove proxy

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
-const express = require('express');
-const cors = require('cors');
-const dt = require('dotenv').config();
-const app = express();
-const path = require('path');
-const fileUpload = require('express-fileupload');
-const port = process.env.PORT || 2917;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use('/', express.static(path.join(__dirname, 'public/')));
-app.use(fileUpload());
-
-// Reverse proxy
-const { createProxyMiddleware } = require('http-proxy-middleware');
-app.use('/', createProxyMiddleware({
-    target: 'https://admin-server-topaz.vercel.app',
-    changeOrigin: true,
-}));
-
+const express = require('express')
+const cors = require('cors')
+const dt = require('dotenv').config()
+const app = express()
+const router = require('./router')
+const path = require('path')
+const fileUpload = require('express-fileupload')
+const port = process.env.PORT || 2917
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use('/', express.static(path.join(__dirname, 'public/')))
+app.use(fileUpload())
+app.use(router)
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
-});
+    console.log(`http://localhost:${port}`)
+})
