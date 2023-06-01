0c9c123b351de67a4549e2b5dd19baa27346b1a4
techkilla2016 <66670136+techkilla2016@users.noreply.github.com>
2023-06-01 16:46:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const fileUpload = require('express-fileupload')
 const port = process.env.PORT || 2917
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(fileUpload())
 app.use(cors(
     {
-        origin: ['https://admin-server-topaz.vercel.app/', 'http://localhost:2917/', 'https://admin-25de.onrender.com/']
+        origin: "https://admin-25de.onrender.com/"
     }
 ))
 app.use('/', express.static(path.join(__dirname, 'public/')))
-app.use(fileUpload())
 app.use(router)
 app.listen(port, () => {
     console.log(`http://localhost:${port}`)
